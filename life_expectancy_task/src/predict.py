a0e1ba6bd6cf13657bbe9434b072c9bc1ecc1da5
Kabir Vohra <kabir.vohra_ug2023@ashoka.edu.in>
2025-09-10 15:41:36 +0530

    files

--- life_expectancy_task/src/predict.py
+++ life_expectancy_task/src/predict.py
@@ -1,3 +1,5 @@
 # the test data has not been provided, and needs to be input here 
+# include the pre-processing steps here as well
+# assume test data is in the same format as train data
 
 test_data = ""      # enter path of the test data here
---
+++ life_expectancy_task/src/train_model.py
@@ -0,0 +1,1 @@
+# implementing linear regression model first: 
