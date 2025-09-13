b03bf77d875024f5533f98bc9d5ba7caa1fc11ea
urvashibala <urvashibala@users.noreply.github.com>
2025-09-13 23:23:59 +0530

    one-hot-encoding-trials
    trying four versions of the code for one hot encoding

--- retail_task/src/data_preprocessing.py
+++ retail_task/src/data_preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.preprocessing as MinMaxScaler
+from sklearn.compose import ColumnTransformer
 
 df = pd.read_csv('/Users/urvashibalasubramaniam/Documents/GitHub/Kabir_Vohra_Urvashi_Balasubraniam_A1/retail_task/src/Retail.csv')
 
@@ -9,18 +10,41 @@ df = pd.read_csv('/Users/urvashibalasubramaniam/Documents/GitHub/Kabir_Vohra_Urv
 Refer data_exploration.ipynb for checks 
  for missing values, var types, statistical distributions etc """
 
-# Correlation analysis
+# Separating numerical and categorical (non-numerical/string) data value columns
+numericalCols = df.select_dtypes(include=["number"]).columns.tolist()
+categoryCols = df.select_dtypes(exclude=["number"]).columns.tolist()
 
+# One hot encoding using scikitlearn
+ct = ColumnTransformer( transformers=[
+        ("cat", MinMaxScaler.OneHotEncoder(handle_unknown='ignore'), categoryCols),
+        ("num", "passthrough", numericalCols) # passthrough = continue 
+    ])
 """
-Need to perform one-hot encoding on the following categorical variables. Of the 77 columns:
-(1) Identifying those that have Strings as fields
-(2) Coming up with a standard rule to perform one-hot encoding without my manual intervention required.
-
-For pandas, we usually use
-uniqueCount = data['category_name'].nunique()
-
-(3) Coming up with a standard rule to perform one-hot encoding without my manual intervention required.
-
-print(f"The number of distinct product IDs is: {unique_products}")
-
+Transformation: 
+cat (transform name) -> apply OneHotEncoder to categoryCols
+num (transform name)-> just pass through without applying anything
+Called later using ct.fit_transform(df) which outputs the numpy array (without headers)
 """
+
+# Call the above transform (outputs numpy array)
+tempEncoded = ct.fit_transform(df)
+
+# Add back the headers and numerical data
+cat_features = ct.named_transformers_["cat"].get_feature_names_out(categoryCols)
+all_features = list(cat_features) + numericalCols
+
+# Making it a dataframe again
+df_encoded = pd.DataFrame(tempEncoded.toarray(), columns=all_features)
+
+print(df_encoded.head())
+
+# Correlation analysis between target and all other variables
+
+# Normalisation (MinMax Scaling) -- try and do this without scikitlearn
+
+# Standardisation
+"""
+Transforms features to have mean = 0 and standard deviation = 1, 
+useful for normally distributed features."""
+
+# Detect and remove outliers
